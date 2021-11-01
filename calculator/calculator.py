c6fb04a250e62d84dc33c6ffbfc3e27243d8dde1
ElReyAzul <rsepulvedam@yahoo.com>
2021-11-01 04:38:33 -0700

    init of repo

---
+++ calculator/calculator.py
@@ -0,0 +1,13 @@
+"""
+Calculator library conintaing basic math operations
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def multiply(first_term, second_term):
+    return first_term * second_term
+
+def substract(first_term, second_term):
+    return first_term - second_term
+
